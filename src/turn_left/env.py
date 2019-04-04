17ed2fe293134ccdfad3aec7acdf97c3c0ba0929
NotAnyMike <mikewoodcockc@gmail.com>
2019-04-04 22:12:23 +0100

    getting the predictions to the left side right

--- src/turn_left/env.py
+++ src/turn_left/env.py
@@ -1,4 +1,5 @@
 import numpy as np
+from pdb import set_trace
 
 from gym.envs.box2d import CarRacing
 from gym.envs.box2d.car_racing import play
@@ -18,22 +19,21 @@ class CarRacing_turn(CarRacing):
     
     def reset(self):
         super(CarRacing_turn,self).reset()
-        # TODO take only t if we are in the second track
         tiles_before = 8
         filter = (env.info['x']) | ((self.info['t']) & (self.info['track'] >0))
         idx = np.random.choice(np.where(filter)[0])
 
         idx_relative = idx - (self.info['track'] < self.info[idx]['track']).sum()
         if env.info[idx]['end']:
-            direction = -1
+            direction = +1
         elif env.info[idx]['start']:
-            direction = 1
+            direction = -1
         else:
             direction = 1 if np.random.random() < 0.5 else -1
-        idx_relative_new = (idx_relative + direction*tiles_before) % len(env.tracks[self.info[idx]['track']])
+        idx_relative_new = (idx_relative - direction*tiles_before) % len(env.tracks[self.info[idx]['track']])
 
         idx_new = idx - idx_relative + idx_relative_new
-        _,beta,x,y = env._get_rnd_position_inside_lane(idx_new,direction=-1*direction)            
+        _,beta,x,y = env._get_rnd_position_inside_lane(idx_new,direction=direction)            
 
         angle_noise = np.random.uniform(-1,1)*np.pi/12
         beta += angle_noise
@@ -42,11 +42,80 @@ class CarRacing_turn(CarRacing):
         speed = np.random.uniform(0,100)
         self.set_speed(0)#speed)
 
+        # adding nodes before coming to the intersection
         self._next_nodes = []
         for i in range(tiles_before):
-            idx_tmp = (idx_relative + direction*i) % len(env.tracks[self.info[idx]['track']])
+            idx_tmp = (idx_relative - direction*i) % len(env.tracks[self.info[idx]['track']])
             idx_tmp = idx - idx_relative + idx_tmp
             self._next_nodes.append({idx_tmp: {0:-direction,1:-direction}})
+
+        inter = self.understand_intersection(idx,direction)
+        
+        def get_avg_d_of_segment(idx,direction):
+            """
+            Calculate the distance of a segment (between two intersections) to
+            the center of the map.
+            """
+            # get the segments
+            intersection_id = self.info[idx]['intersection_id']
+            track_id = self.info[idx]['track']
+            intersections_nodes = np.where((self.info['intersection_id'] != -1) & (self.info['track'] == track_id))[0]
+            tmp_pos = np.where(intersections_nodes == idx)[0][0]
+            if direction == 1:
+                end = idx
+                start = intersections_nodes[(tmp_pos-1)%len(intersections_nodes)]
+            else:
+                start = idx
+                end = intersections_nodes[(tmp_pos+1)%len(intersections_nodes)]
+
+            print(start,end)
+            ids = []
+            if start > end:
+                ids = []
+                #set_trace()
+                len_other_tracks = (self.info['track'] < track_id).sum()
+                len_track = len(self.tracks[track_id])
+                start = start-len_other_tracks
+                end   = end-len_other_tracks
+                while start != end:
+                    ids.append(len_other_tracks+start)
+                    start = (start+1)%len_track
+                segment = self.track[ids][:,1,2:]
+            else:
+                ids = list(range(start,end))
+                segment = self.track[ids,1,2:]
+            avg_d = np.mean(np.linalg.norm(segment, axis=1))
+            return avg_d,ids
+
+        ######## adding nodes after the intersection
+        # get avg distance from the origin
+        intersection_id = self.info[idx]['intersection_id']
+        current_track = self.info[idx]['track']
+        node_id_main_track = np.where((self.info['intersection_id'] == intersection_id) & (self.info['track'] == 0))[0][0]
+        node_id_second_track = np.where((self.info['intersection_id'] == intersection_id) & (self.info['track'] == 1))[0]
+        if direction == 1:
+            node_id_second_track = node_id_second_track[-1]
+        else:
+            node_id_second_track = node_id_second_track[0]
+
+        avg_main_track,ids= get_avg_d_of_segment(node_id_main_track, direction)
+        # Debugging purposes
+        #for i in ids:
+        #   self._next_nodes.append({i:{0:direction,1:direction}})
+
+        avg_second_track,ids = get_avg_d_of_segment(node_id_second_track, direction)
+        # Debugging purposes
+        #for i in ids:
+        #    self._next_nodes.append({i:{0:direction,1:direction}})
+
+        # The direction of left lane
+        flow = -1 if avg_second_track > avg_main_track else 1
+        if current_track == 0: flow *= -1
+        if inter['left'] is not None:
+            for i in range(tiles_before):
+                self._next_nodes.append({inter['left']+flow*(i):{0:direction,1:direction}})
+        ###########
+
 
     def _remove_prediction(self, id,lane,direction):
         pass
@@ -56,7 +125,15 @@ class CarRacing_turn(CarRacing):
 
     def step(self,action):
         # TODO check if it is time to reset 
+        #set_trace()
         return super(CarRacing_turn,self).step(action)
+
+    def remove_current_tile(self, id, lane):
+        if id in self._current_nodes:
+            if len(self._current_nodes[id]) > 1:
+                del self._current_nodes[id][lane]
+            else:
+                del self._current_nodes[id]
 
 if __name__=='__main__':
     env = CarRacing_turn()
