84b89c41276ee1413faca6d6ca67dc3e1dab207b
NotAnyMike <mikewoodcockc@gmail.com>
2019-04-13 11:05:05 +0100

    adding run_model file

---
+++ hrl/common/run_model.py
@@ -0,0 +1,53 @@
+from pdb import set_trace
+import glob
+import os
+
+import numpy as np
+from stable_baselines.common.vec_env import DummyVecEnv
+from stable_baselines import PPO2
+
+from hrl.common.arg_extractor import get_load_args
+from hrl.envs import env as environments
+
+def load_model(
+        experiment,
+        folder='experiments', 
+        weights=None,
+        env='Base',
+        ):
+
+    if folder[-1] in '\\/':
+        # remove \ from the end
+        folder = folder[:-1]
+
+    if weights is None:
+        # Check what is the last weight
+        weights_lst = [s for s in os.listdir('/'.join([folder,experiment])) if "weights_" in s]
+        weights_lst = [s.replace('weights_','').replace('.pkl','') for s in weights_lst]
+        if 'final' in weights_lst:
+            weights = 'weights_final.pkl'
+        else:
+            weights_lst = [int(s) for s in weights_lst]
+            weights = 'weights_' + str(max(weights_lst)) + '.pkl'
+    
+    weights_loc = '/'.join([folder,experiment,weights])
+
+    # Get env
+    env = getattr(environments, env)
+    env = DummyVecEnv([env])
+
+    model = PPO2.load(weights_loc)
+    model.set_env(env)
+
+    obs = env.reset()
+    while True:
+        action, _states = model.predict(obs)
+        obs, rewards, dones, info = env.step(action)
+        env.render()
+
+if __name__ == '__main__':
+    # Run arg parser
+    args = get_load_args()
+
+    # Run run experiment
+    load_model(**args)
