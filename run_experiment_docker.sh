7850837f97b003d91ad4b66f59da23fd6721c8ce
NotAnyMike <mikewoodcockc@gmail.com>
2019-05-31 11:05:21 +0100

    adding pull to run command from docker

--- run_experiment_docker.sh
+++ run_experiment_docker.sh
@@ -7,6 +7,10 @@ echo "Executing in the docker (gpu image):"
 echo $cmd_line
 
 
-sudo docker run -it --runtime=nvidia --rm --network host --ipc=host \
+#sudo docker run -it --runtime=nvidia --rm --network host --ipc=host \
+sudo docker run -it --runtime=nvidia --rm \
   --mount src=$(pwd)/experiments,target=/HRL/outside_experiments,type=bind notanymike/hrl_entry18 \
-  bash -c "cd /HRL && $cmd_line"
+  bash -c "cd /stable-baselines && git reset --hard && git pull && 
+	cd /gym && git reset --hard && git pull && 
+	cd /HRL && git reset --hard && git pull &&
+	$cmd_line"
