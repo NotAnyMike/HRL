0ebbe755f4b236c293e9caa00ae87c7640c91ad9
NotAnyMike <mikewoodcockc@gmail.com>
2019-06-03 08:07:16 +0100

    updating shell script

--- run_experiment_docker.sh
+++ run_experiment_docker.sh
@@ -10,7 +10,7 @@ echo $cmd_line
 nvidia-docker run -it --rm \
   --mount src=$(pwd)/experiments,target=/HRL/outside_experiments,type=bind \
   --mount src=$(pwd)/tracks,target=/HRL/tracks,type=bind \
-  hrl:entry-16.04 \
+  hrl:16.04 \
   bash -c "cd /stable-baselines && git reset --hard && git pull && 
 	cd /gym && git reset --hard && git pull && 
 	cd /HRL && git reset --hard && git pull &&
