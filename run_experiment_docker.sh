6b87521abaa4f4b2d45a23e2b057e5a89d387b71
notanymike <mikewoodcockc@gmail.com>
2019-06-05 22:33:19 +0100

    updating container for nvidia-docker2

--- run_experiment_docker.sh
+++ run_experiment_docker.sh
@@ -7,10 +7,11 @@ echo "Executing in the docker (gpu image):"
 echo $cmd_line
 
 
-nvidia-docker run -it --rm \
+#sudo docker run -it --runtime=nvidia --rm --network host --ipc=host \
+docker run --runtime=nvidia -ti --rm \
   --mount src=$(pwd)/experiments,target=/HRL/outside_experiments,type=bind \
   --mount src=$(pwd)/tracks,target=/HRL/tracks,type=bind \
-  hrl:16.04 \
+  hrl:18.04 \
   bash -c "cd /stable-baselines && git reset --hard && git pull && 
 	cd /gym && git reset --hard && git pull && 
 	cd /HRL && git reset --hard && git pull &&
