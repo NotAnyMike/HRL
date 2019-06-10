038d72ec17da2792be568888e1a358826232d843
NotAnyMike <mikewoodcockc@gmail.com>
2019-06-10 08:54:39 +0100

    correcting max_step for terminatin for KL

--- hrl/policies/policy.py
+++ hrl/policies/policy.py
@@ -5,9 +5,9 @@ from stable_baselines import PPO2
 from stable_baselines.common.vec_env import DummyVecEnv
 
 class Policy:
-    def __init__(self,weights,id=None):
+    def __init__(self,weights,id=None,max_steps=40):
         self.model = PPO2.load(weights)
-        self.max_steps = 40
+        self.max_steps = max_steps
         self.n = 0
         self.id = id
 
@@ -118,5 +118,8 @@ class X(HighPolicy):
 
 
 class Keep_lane(Policy):
-    def __init__(self):
-        super(Keep_lane, self).__init__("hrl/weights/Keep_lane/v1.0.pkl",id='KL')
+    def __init__(self,max_steps=10):
+        super(Keep_lane, self).__init__(
+                "hrl/weights/Keep_lane/v1.0.pkl",
+                id='KL',
+                max_steps=max_steps,)
